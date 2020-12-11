bc678215d0441ed9beed908004fea9e7672fb7f8
Ian Marshall <imarshall14@gmail.com>
2020-12-11 18:20:15 -0500

    commit tree 5f1fd8cbae01b79b8df11ddf6708d9ab04475a73

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+require "pry"
 
+def sort_array_asc (array)
+    array.sort
+end
+
+def sort_array_desc (array)
+    array.sort.reverse
+end
+
+def sort_array_char_count (array)
+    array.sort do |a,b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements (array) #this is probably not the method they want
+    new_second = array[2]
+    new_third = array [1]
+    array[1] = new_second
+    array[2] = new_third
+    array
+end
+
+def reverse_array (array)
+    array.reverse
+end
+
+def kesha_maker (array)
+    new_array = []
+    final_array =[]
+    array.map do |word|
+        new_array = word.split("")
+        word.split("")
+        new_array[2] = "$"
+        new_array.join
+    end
+end
+
+def find_a (array)
+    array.select do |i| 
+    i.start_with? "a"
+    end
+end
+
+def sum_array (array)
+    array.inject {|sum, number| sum + number}
+end
+
+
+def add_s (array)
+    array.each_with_index.collect do|element, index|
+        element == array[1] ? element : element.concat("s")
+    end
+end
